68c23f75bf046302a78d63a95ad718dcae5e090f
VishamPersaud <vishamp0@gmail.com>
2024-07-31 22:31:34 +0000

    This is my homework on JavaScript Madlibs

--- script.js
+++ script.js
@@ -8,4 +8,4 @@ let truth = false
 
 let yourname = prompt("Enter your name")
 
-console.log(yourname + " and "+ name + " just turn " + age + " and having a good time at there birthday party. Until the Avengers and Justice League came crashing town your mansion killing " + death + " people. Leaving only you alive to tell the " + truth + " hood of we are living in a simulation." )
+console.log(yourname + " and "+ name + " just turn " + age + " and having a good time at there birthday party. Until the Avengers and Justice League came crashing down your mansion killing " + death + " people. Leaving only you alive to tell the " + truth + " hood of we are living in a simulation." )

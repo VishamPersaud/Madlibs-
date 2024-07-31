a3f6edf60048be10b66ff61c7df62da3d0db7a96
VishamPersaud <vishamp0@gmail.com>
2024-07-31 22:27:01 +0000

    This is my homework on JavaScript Madlibs

---
+++ script.js
@@ -0,0 +1,11 @@
+ 
+//Global Variables 
+
+let name = "Wade"
+let age = 21 
+let death = 10*10
+let truth = false 
+
+let yourname = prompt("Enter your name")
+
+console.log(yourname + " and "+ name + " just turn " + age + " and having a good time at there birthday party. Until the Avengers and Justice League came crashing town your mansion killing " + death + " people. Leaving only you alive to tell the " + truth + " hood of we are living in a simulation." )
